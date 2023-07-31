9e8d1fd285b8ba7b937cf659f19ce238bee3d468
Pavlo <pavel.levcenko05@gmail.com>
2023-07-31 10:22:13 +0200

    add functionality to serve static images

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { getProducts } from './controllers/phone';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,8 @@ app.use(
   })
 );
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/products', getProducts);
 
