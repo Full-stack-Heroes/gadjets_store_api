02bea23a082cf007932fbdf5dd5c3033bc44eabf
Pavlo <pavel.levcenko05@gmail.com>
2023-08-04 13:24:17 +0200

    deleted build folder

--- build/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.models = void 0;
-const product_model_1 = require("./product.model");
-const phones_model_1 = require("./phones.model");
-const tablets_model_1 = require("./tablets.model");
-const accessories_model_1 = require("./accessories.model");
-exports.models = [product_model_1.Product, phones_model_1.Phones, tablets_model_1.Tablets, accessories_model_1.Accessories];
