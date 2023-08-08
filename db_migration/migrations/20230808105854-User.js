354810aa0a2e95c3cd2af695cfaf87de2e6185d9
Stepan Haida <stepanhaida@gmail.com>
2023-08-08 17:17:36 +0300

    add migration

---
+++ db_migration/migrations/20230808105854-User.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const TABLE_NAME = 'Users';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable(TABLE_NAME, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      username: {
+        allowNull:false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      role: {
+        defaultValue: 'User',
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable(TABLE_NAME);
+  }
+};
